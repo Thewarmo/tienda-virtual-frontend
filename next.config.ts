e261cf848b09ef827cba08b1e936372fb9b79c11
Thewarmo <abayona123@hotmail.com>
2025-08-26 13:29:06 -0500

    ajuste next.config para imagenes del sitio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'tienda-virtual-catalogo-afb-20250814.s3.us-east-1.amazonaws.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'tienda-virtual-insumos-production.up.railway.app',
+          port: '',
+          pathname: '/**',
+        }
+      ],
+    },
+  }
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
